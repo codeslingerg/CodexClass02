eddbb9b21b27b29c601a8f11cbf21700ca9d7a61
Gerard <gerard.g.johnson@gmail.com>
2023-04-15 17:20:50 -0500

    commit added css,index, and js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # lab-02
+
+This is an about page about me. It will ask 5 questions and give me a response to each question. 
+
+## Author
+
+by Gerard Johnson
+
+## Collaboration
+
+Working together with the class.
+
+## Lighthouse Score
+
+TODO: take a screenshot of the Lighthouse score. 
---
+++ js/app.js
@@ -0,0 +1,51 @@
+'use strict'
+//TODO: ask the user's name.
+
+//TODO: Ask 5 y/n questions
+let answerOne = prompt("Did I take a web design class?");
+let answerTwo = prompt("Can I speak another language?");
+let answerThree = prompt("Is my favorite color yellow");
+let answerFour = prompt("Have I traveled outside of the US");
+let answerFive = prompt("Am I left-handed?")
+
+//use if / else
+//if 'y' then you are correct
+
+if(answerOne === 'y' || answerOne === 'yes'){
+    console.log("(¡Así es!)That's right!");
+} else {
+    // if 'n' then you are incorrect
+    console.log("(Está mal) That's wrong.");
+}
+
+if(answerTwo === 'y' || answerTwo === 'yes'){
+    console.log("(¡Así es!)That's right!");
+} else {
+    // if 'n' then you are incorrect
+    console.log("(Está mal) That's wrong.");
+}
+
+if(answerThree === 'n' || answerThree === 'no'){
+    console.log("(¡Así es!)That's right!");
+} else {
+    // if 'y' then you are incorrect
+    console.log("(Está mal) That's wrong.");
+}
+
+if(answerFour === 'n' || answerFour === 'no'){
+    console.log("(¡Así es!)That's right!");
+} else {
+    // if 'n' then you are incorrect
+    console.log("(Está mal) That's wrong.");
+}
+
+if(answerFive === 'y' || answerFive === 'yes'){
+    console.log("(¡Así es!)That's right!");
+} else {
+    // if 'n' then you are incorrect
+    console.log("(Está mal) That's wrong.");
+}
+
+//TODO: Give a response with an alert. 
+
+// Notify user if they are correct. 
